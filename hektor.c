c6f48d26d86fdad4beb86406a38ffc78eed2479e
Mick Koch <kchmck@gmail.com>
2010-04-23 20:30:47 -0400

    Add gplv3

--- hektor.c
+++ hektor.c
@@ -1,3 +1,19 @@
+// Copyright 2010 Mick Koch <kchmck@gmail.com>
+//
+// This file is part of Hektor.
+// 
+// Hektor is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later
+// version.
+// 
+// Hektor is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+// A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with
+// Hektor.  If not, see <http://www.gnu.org/licenses/>.
+
 #include "hektor.h"
 
 bool hektor_cmd_help(hektor_t *hektor) {
